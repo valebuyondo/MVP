b5753ec2558bcf752f11087e1c36427b69bb29ef
valebuyondo <buyondovale@gmail.com>
2024-03-04 12:29:00 -0800

    app.js update

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
@@ -10,7 +11,13 @@ app.use(express.json());
 const assetRoute = require('./routes/asset');
 app.use('/assets', assetRoute);
 
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/asset-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB', err));
+
 // Start server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
