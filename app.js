5163a0661a2d99f106605a9d3f67f3bb4bea16a5
valebuyondo <buyondovale@gmail.com>
2024-03-04 12:20:28 -0800

    update

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const app = express();
 const port = 3000;
 
 // Middleware
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
-//
-// // Routes
-// const assetRoute = require('./routes/asset');
-// app.use('/assets', assetRoute);
-//
-// // Start server
-// app.listen(port, () => {
-//   console.log(`Server is running on http://localhost:${port}`);
-//   });
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Routes
+const assetRoute = require('./routes/asset');
+app.use('/assets', assetRoute);
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
