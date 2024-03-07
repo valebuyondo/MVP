7adb5fb4284b2680c20256a82bd617d72d250f50
admin <admin@DPM-LTP-1.ppda.go.ug>
2024-03-07 12:18:51 +0300

    public folder

--- app.js
+++ app.js
@@ -6,10 +6,17 @@ const port = 3000;
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 const assetRoute = require('./routes/asset');
 app.use('/assets', assetRoute);
+
+// Define a route for the root URL ("/") to serve the index.html file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost/asset-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
