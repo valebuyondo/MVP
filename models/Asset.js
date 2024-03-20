57bc3e0a792bd2e8cd56d2a62a4672ab4268fbcc
valebuyondo <buyondovale@gmail.com>
2024-03-20 18:38:10 +0300

    create a databse schema

--- models/Asset.js
+++ models/Asset.js
@@ -1,11 +1,10 @@
+// models/Asset.js
 const mongoose = require('mongoose');
 
 const assetSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  description: String
+  name: { type: String, required: true },
+  description: { type: String },
+  // Add other fields as needed
 });
 
 module.exports = mongoose.model('Asset', assetSchema);
