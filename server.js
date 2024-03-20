85fc52c11e4d9459da5a36cbfa26fbd3a517e4a5
valebuyondo <buyondovale@gmail.com>
2024-03-20 18:34:21 +0300

    creating as a bakend server

---
+++ server.js
@@ -0,0 +1,29 @@
+// server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.json());
+
+// MongoDB connection
+mongoose.connect('mongodb://localhost:27017/asset-tracker', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Define routes
+app.use('/api/assets', require('./routes/assetRoutes'));
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
